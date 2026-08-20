4b0f49e654420fa40cdc3e94a9f3a54798b85447
Abhimanyu Tripathi <abhimanyutripathi2002@gmail.com>
2026-08-20 11:38:33 +0530

    Day 3 pushed

--- 01-Python/week-01/variables.py
+++ 01-Python/week-01/variables.py
@@ -10,3 +10,5 @@ labour_m1 = "John"
 is_home = True
 
 print(type(length_of_land), type(bricks_cost_per_piece), type(labour_m1), type(is_home))
+
+print("Bricks cost per unit is {}".format(bricks_cost_per_piece))
